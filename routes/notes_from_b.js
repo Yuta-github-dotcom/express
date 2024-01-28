1a415f76b1a4c43211bb2d4340bff154571816fb
Yuta-github-dotcom <ochappytime@gmail.com>
2024-01-28 17:39:58 +0900

    first commit

--- routes/notes_from_b.js
+++ routes/notes_from_b.js
@@ -3,10 +3,9 @@ var router = express.Router();
 const cors = require('cors'); // corsミドルウェアを追加
 require('dotenv').config();
 
-
 // 接続情報を設定
 const { MongoClient } = require("mongodb");
-const uri = "mongodb+srv://yuta:Lena0702Vi2@cluster0.sjozhka.mongodb.net/?retryWrites=true&w=majority";
+const uri = process.env.MONGODB_URI;
 const client = new MongoClient(uri);
 
 // corsミドルウェアを使用
